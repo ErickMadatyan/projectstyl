c7eb0f9d6a18e3a4db9fa7610de76965d3629893
ErickMadatyan <89811461+ErickMadatyan@users.noreply.github.com>
2023-10-25 21:52:53 -0700

    Update signup.inc.php

--- signup.inc.php
+++ signup.inc.php
@@ -26,7 +26,7 @@ if (pwdMatch($pwd, $pwdRepeat) !== false) {
     header("location: signup.php?error=passwordsdontmatch");
     exit();
 }
-if (uidExists($conn, $username) !== false) {
+if (uidExists($conn, $username, $email) !== false) {
     header("location: signup.php?error=usernametaken");
     exit();
 }
