b01541dc1338907fa6e4013000f5b04ad499e904
ErickMadatyan <89811461+ErickMadatyan@users.noreply.github.com>
2023-10-29 16:11:55 -0700

    Update and rename settings.html to settings.php

---
+++ settings.php
@@ -0,0 +1,14 @@
+<?php
+  include 'header.php';
+?>
+<!------------ CONTENT ------------->  
+  <body>
+  <div class="content-wrap">
+    <div class="column-left">
+
+    </div>  
+  </div>    
+    <footer class="footer">
+    </footer>
+  </body>
+</html>
