9343f4dc1e2f1c186d7eaed6a73eef7bf1551716
ErickMadatyan <89811461+ErickMadatyan@users.noreply.github.com>
2023-10-29 15:23:02 -0700

    Create reset-password.php

---
+++ reset-password.php
@@ -0,0 +1,22 @@
+<?php
+  include 'header.php';
+?>
+<!------------ CONTENT ------------->   
+<section class="signup-form">
+<h2>
+  Reset your password
+  </h2>
+  <p>An E-mail will be sent with instructions to reset your password</p>
+  <form action="reset-request.inc.php" method="post">
+    <input type="text" name="email" placeholder="Enter your E-mail Address...">
+    <button type="submit" name="reset-request-submit">Receive New Password By Email</button>
+  </form>
+
+</section>
+
+
+
+    <footer class="footer">
+    </footer>
+  <script src="script.js"></script>
+</html>
