f6533efcc7e5c5e7153dcad9aa027621d9a14ad9
ErickMadatyan <89811461+ErickMadatyan@users.noreply.github.com>
2023-10-29 00:59:51 -0700

    Update signup.inc.php

--- signup.inc.php
+++ signup.inc.php
@@ -5,7 +5,7 @@ if (isset($_POST["submit"])){
     $email = $_POST["email"];
     $username = $_POST["uid"];
     $pwd = $_POST["pwd"];
-    $pwdRepeat = $_POST["pwdrepeat"];
+    $pwdRepeat = $_POST["pwdRepeat"];
 
     require_once 'dbh.inc.php';
     require_once 'functions.inc.php';
