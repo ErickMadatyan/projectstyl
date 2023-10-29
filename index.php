33686e3f1b09690fe61eb5e1bd29d7ea7a56a7c2
ErickMadatyan <89811461+ErickMadatyan@users.noreply.github.com>
2023-10-29 14:19:55 -0700

    Update index.php

--- index.php
+++ index.php
@@ -20,6 +20,12 @@
   <div class="content-wrap">
     <div class="column-left">
 
+              <?php
+          if(isset($_SESSION["useruid"])) {
+            echo "<p>Hello There" . $_SESSION["useruid"] . "</p>";
+          }
+        ?>
+
       <h1>
         Home page
       </h1>
