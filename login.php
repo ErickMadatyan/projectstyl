6602e3cc50d8c97646d614a1dbdbf9eba8730a0c
ErickMadatyan <89811461+ErickMadatyan@users.noreply.github.com>
2023-10-29 14:05:20 -0700

    Update login.php

--- login.php
+++ login.php
@@ -7,7 +7,7 @@
   Log In
   </h2>
   <form action="login.inc.php" method="post">
-    <input type="text" name="name" placeholder="Username/Email...">
+    <input type="text" name="uid" placeholder="Username/Email...">
     <input type="password" name="pwd" placeholder="Password...">
     <button type="submit" name="submit">
       Log In
