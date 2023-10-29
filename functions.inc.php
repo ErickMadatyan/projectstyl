73014601ffd964ee1e1dba16843d97751b4ef780
ErickMadatyan <89811461+ErickMadatyan@users.noreply.github.com>
2023-10-29 01:08:29 -0700

    Update functions.inc.php

--- functions.inc.php
+++ functions.inc.php
@@ -1,7 +1,7 @@
 <?php
 function emptyInputSignup($name, $email, $username, $pwd, $pwdRepeat) {
     $result;
-    if (empty($name) || (empty($email) || (empty($username) || (empty($pwd) || (empty($pwdRepeat)) {
+    if (empty($name) || empty($email) || empty($username) || empty($pwd) || empty($pwdRepeat)) {
         $result = true;
     }
     else {
