809f9cdbcba73efd459a843ef1a6e4b6b3606f2c
ErickMadatyan <89811461+ErickMadatyan@users.noreply.github.com>
2023-11-06 13:51:04 -0800

    Update upload.php

--- upload.php
+++ upload.php
@@ -1,63 +1,6 @@
-<!DOCTYPE html>
-<html lang="en">
-  <head>
-    <meta charset="utf-8" />
-    <meta name="viewport" content="width=device-width, initial-scale=1" />
-    <link rel="icon" href="https://glitch.com/favicon.ico" />
-    <title>Outfit Creator</title>
-    <link rel="stylesheet" href="style.css" />
-    <script src="https://kit.fontawesome.com/86af3d0397.js" crossorigin="anonymous"></script> <!-- fontawesome -->
-  </head>
-  
-<!------------ Top Navigation ------------->   
-<header>
-  <div class="topnav">
-    <div class="left">
-      <a href="#"><img src="https://cdn.glitch.global/4ac967db-08b7-4f2f-9f47-431c462bd179/logo2.png?v=1697079907040" alt="Logo" width="200px">             </a>
-    </div>
-    <div class="center">
-          <h1>
-        Outfit Creator
-      </h1>
-          </div>
-                <div class="right">
-        <a href="#" id="loginButton"></a>
-      </div>
-
-  </div>
-
-<!------------ Side Navigation -------------> 
-    <div class="sidenavcontent">
-      <div class="sidenav">
-          <a href="index.html" class="nav-link">
-            <div class="nav-box">
-                <span>Home</span>
-            </div></a>
-          <a href="explore.html" class="nav-link">
-            <div class="nav-box">
-                <span>Explore</span>
-            </div></a>
-                  <a href="upload.html" class="nav-link">
-            <div class="nav-box">
-                <span>Create Outfit</span>
-            </div></a>
-                  <a href="profile.html" class="nav-link">
-            <div class="nav-box">
-                <span>Your Profile</span>
-            </div></a>
-        
-        <div class="bottom-link">  
-          <a href="about.html">About Us</a>
-          <a href="tos.html">Terms of Service</a>
-          
-          <div class ="center">
-             <p>Copyright PROJECT STYL</p>
-          </div>
-        </div>        
-      </div>
-    </div>
-     
-  </header>
+<?php
+  include 'header.php';
+?>
 <!------------ CONTENT ------------->  
 
 
