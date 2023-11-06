4156e18ca243d5699145dfec9c838f484010fc0f
ErickMadatyan <89811461+ErickMadatyan@users.noreply.github.com>
2023-11-06 12:58:14 -0800

    Update index.php

--- index.php
+++ index.php
@@ -4,19 +4,6 @@
 <!------------ CONTENT ------------->   
   <body>
 
-        <!-- Login Form (hidden by default) -->
-    <div class="login-modal" id="loginModal">
-        <div class="login-content">
-            <span class="close" id="closeButton">&times;</span>
-            <h2>Login</h2>
-            <form>
-                <input type="text" placeholder="Username">
-                <input type="password" placeholder="Password">
-                <button type="submit">Login</button>
-            </form>
-        </div>
-    </div>
-
   <div class="content-wrap">
     <div class="column-left">
 
