d3b42ef39b766d485158140b7d782ee87edbf431
ErickMadatyan <89811461+ErickMadatyan@users.noreply.github.com>
2023-11-06 12:59:06 -0800

    Update explore.php

--- explore.php
+++ explore.php
@@ -3,18 +3,7 @@
 ?>
 <!------------ CONTENT ------------->  
   <body>    
-            <!-- Login Form (hidden by default) -->
-    <div class="login-modal" id="loginModal">
-        <div class="login-content">
-            <span class="close" id="closeButton">&times;</span>
-            <h2>Login</h2>
-            <form action="login.php" method="post">
-                <input type="text" placeholder="Username">
-                <input type="password" placeholder="Password">
-                <button type="submit">Login</button>
-            </form>
-        </div>
-    </div>
+
   <div class="content-wrap">
     <div class="column-left">
 
