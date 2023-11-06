ddc34d6711299fdd44311f4c5673d340e7667d04
ErickMadatyan <89811461+ErickMadatyan@users.noreply.github.com>
2023-11-06 14:36:40 -0800

    Update upload.php

--- upload.php
+++ upload.php
@@ -28,17 +28,7 @@
     </div>
 
 
-      <footer class="footer">    
-      <div>
-        <h4>Questions? Email us!</h4>
-      </div>
-        <div class="contact-info">
-          <div>
-            <a href="project.styl.inc@gmail.com"><i class="fa-solid fa-envelope"></i></a><a href="project.styl.inc@gmail.com">project.styl.inc@gmail.com</a><a href="project.styl.inc@gmail.com"><i class="fa-solid fa-envelope"></i></a>
-          </div>
-        </div>
-    </footer>
-</body>
-      <script src="script.js"></script>
-
+<?php
+  include 'header.php';
+?>
 </html>
