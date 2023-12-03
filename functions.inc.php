f00fff9b22c1753ccf2029e247c6d08a304f3bc0
ErickMadatyan <89811461+ErickMadatyan@users.noreply.github.com>
2023-12-03 14:31:55 -0800

    Update functions.inc.php

--- functions.inc.php
+++ functions.inc.php
@@ -109,7 +109,7 @@ function loginUser($conn, $username, $pwd) {
         session_start();
         $_SESSION["userid"] = $uidExists["usersId"];
         $_SESSION["useruid"] = $uidExists["usersUid"];
-        header("location: index.php");
+        header("location: profile.php");
         exit();
     }
 }
