b77f5a21d3fea0d673508682b7743e476e934a7d
ErickMadatyan <89811461+ErickMadatyan@users.noreply.github.com>
2023-12-03 00:35:19 -0800

    Update profile.php

--- profile.php
+++ profile.php
@@ -9,24 +9,48 @@
     </div>  
   </div>  
     <?php
-require_once ('PHPMailer/PHPMailerAutoload.php');
+use PHPMailer\PHPMailer\PHPMailer;
+use PHPMailer\PHPMailer\SMTP;
+use PHPMailer\PHPMailer\Exception;
 
+require './PHPMailer/src/Exception.php';
+require './PHPMailer/src/PHPMailer.php';
+require './PHPMailer/src/SMTP.php';
+
+//Create an instance; passing `true` enables exceptions
 $mail = new PHPMailer(true);
-$mail->isSMTP();
-$mail->SMTPAuth = true;
-$mail->SMTPSecure = 'ssl';
-$mail->Host = 'smtp.gmail.com';
-$mail->Port = '465';
-$mail->isHTML();
-$mail->Username = 'project.styl.inc@gmail.com';
-$mail->Password = 'Dark30death';
-$mail->SetForm('no-reply@projectstyl.com');
-$mail->Subject = 'Hello World';
-$mail->Body = 'A test Email!';
-$mail->AddAddress('madatyanerick@gmail.com');
-$mail->Send();
 
+try {
+    //Server settings
+    $mail->isSMTP();                                            //Send using SMTP
+    $mail->Host       = 'smtp.gmail.com';                     //Set the SMTP server to send through
+    $mail->SMTPAuth   = true;                                   //Enable SMTP authentication
+    $mail->Username   = 'project.styl.inc@gmail.com';                     //SMTP username
+    $mail->Password   = 'fwttnopuzhamhtry';                               //SMTP password
+    $mail->SMTPSecure = PHPMailer::ENCRYPTION_SMTPS;            //Enable implicit TLS encryption
+    $mail->Port       = 465;                                    //TCP port to connect to; use 587 if you have set `SMTPSecure = PHPMailer::ENCRYPTION_STARTTLS`
+
+    //Recipients
+    $mail->setFrom('no-reply@projectstyl.com', 'Mailer');
+    $mail->addAddress('madatyanerick@gmail.com');     //Add a recipient
+
+
+
+    //Content
+    $mail->isHTML(true);                                  //Set email format to HTML
+    $mail->Subject = 'Here is the subject';
+    $mail->Body    = 'This is the HTML message body <b>in bold!</b>';
+    $mail->AltBody = 'This is the body in plain text for non-HTML mail clients';
+
+    $mail->send();
+    echo 'Message has been sent';
+} catch (Exception $e) {
+    echo "Message could not be sent. Mailer Error: {$mail->ErrorInfo}";
+}
+  
 ?>
+
+    
 <?php
   include 'footer.php';
 ?>
