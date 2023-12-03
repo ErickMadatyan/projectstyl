64fb7dcfbea3439f86ee58e8c93289386d0c0bd1
ErickMadatyan <89811461+ErickMadatyan@users.noreply.github.com>
2023-12-02 20:08:20 -0800

    Update profile.php

--- profile.php
+++ profile.php
@@ -5,7 +5,24 @@
   <body>  
   <div class="content-wrap">
     <div class="column-left">
-hello
+<?php
+require_once('PHPMailer/PHPMailerAutoload.php');
+
+$mail = new PHPMailer();
+$mail->isSMTP();
+$mail->SMTPAuth = true;
+$mail->SMTPSecure = 'ssl';
+$mail->Host = 'smtp.gmail.com';
+$mail->Port = '465';
+$mail->isHTML();
+$mail->Username = 'project.styl.inc@gmail.com';
+$mail->Password = 'Dark30death';
+$mail->SetForm('no-reply@projectstyl.com');
+$mail->Subject = 'Hello World';
+$mail->Body = 'A test Email!';
+$mail->AddAddress('madatyanerick@gmail.com');
+$mail->Send();
+?>
     </div>  
   </div>  
 <?php
