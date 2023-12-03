3fae26eb3f84099d4a1018ccbc43944bbe9782c5
ErickMadatyan <89811461+ErickMadatyan@users.noreply.github.com>
2023-12-03 02:50:16 -0800

    Update signup.php

--- signup.php
+++ signup.php
@@ -1,100 +1,92 @@
 <?php
-  include 'header.php';
+include 'header.php';
 ?>
-    <script src="https://www.google.com/recaptcha/api.js" async defer></script>
+<script src="https://www.google.com/recaptcha/api.js" async defer></script>
 <!------------ CONTENT ------------->   
 <section class="signup-form">
-<h2>
-  Sign Up
-  </h2>
-  <form action="signup.inc.php" method="post">
-    <input type="text" name="name" placeholder="Full Name...">
-    <input type="text" name="email" placeholder="Email...">
-    <input type="text" name="uid" placeholder="Username...">
-    <input type="password" name="pwd" id="password" placeholder="Password..." oninput="checkPasswordStrength()">
-    <span id="password-strength"></span>
-    <input type="password" name="pwdRepeat" placeholder="Repeat Password...">
-    <div class="g-recaptcha" data-sitekey="6LeEkiQpAAAAADXkInBDRN-5HhcoGAB5Nl2Z1mkk"></div>
-    <button type="submit" name="submit">Sign Up</button>
-  </form>
-  <?php
-  if(isset($_GET["error"])) {
-    if($_GET["error"] == "emptyinput") {
-      echo "<p> Fill in all fields! </p>";
+    <h2>Sign Up</h2>
+    <form action="signup.inc.php" method="post">
+        <input type="text" name="name" placeholder="Full Name...">
+        <input type="text" name="email" placeholder="Email...">
+        <input type="text" name="uid" placeholder="Username...">
+        <input type="password" name="pwd" id="password" placeholder="Password..." oninput="checkPasswordStrength()">
+        <span id="password-strength"></span>
+        <input type="password" name="pwdRepeat" placeholder="Repeat Password...">
+        <div class="g-recaptcha" data-sitekey="6LeEkiQpAAAAADXkInBDRN-5HhcoGAB5Nl2Z1mkk"></div>
+        <button type="submit" name="submit">Sign Up</button>
+    </form>
+    <?php
+    if (isset($_GET["error"])) {
+        if ($_GET["error"] == "emptyinput") {
+            echo "<p> Fill in all fields! </p>";
+        } else if ($_GET["error"] == "invaliduid") {
+            echo "<p> Choose a proper username! </p>";
+        } else if ($_GET["error"] == "invalidemail") {
+            echo "<p> Choose a proper email! </p>";
+        } else if ($_GET["error"] == "passwordsdontmatch") {
+            echo "<p> Passwords don't match! </p>";
+        } else if ($_GET["error"] == "stmtfailed") {
+            echo "<p> Something went wrong, please try again! </p>";
+        } else if ($_GET["error"] == "usernametaken") {
+            echo "<p> Username already taken! </p>";
+        } else if ($_GET["error"] == "recaptchafailed") {
+            echo "<p> reCAPTCHA verification failed! Please try again. </p>";
+        }
     }
-    else if ($_GET["error"] == "invaliduid") {
-      echo "<p> Choose a proper usename! </p>";
-    }
-    else if ($_GET["error"] == "invalidemail") {
-      echo "<p> Choose a proper email! </p>";
-    }
-    else if ($_GET["error"] == "passwordsdontmatch") {
-      echo "<p> passwords dont match! </p>";
-    }
-    else if ($_GET["error"] == "stmtfailed") {
-      echo "<p> Something went wrong try again! </p>";
-    }
-    else if ($_GET["error"] == "usernametaken") {
-      echo "<p> username already taken! </p>";
-    }
-    else if ($_GET["error"] == "none") {
-      echo "<p> you have signed up! </p>";
-    }
-  }
-?>
+    ?>
     <?php
     if (isset($_GET["newpwd"])) {
-      if ($_GET["newpwd"] == "passwordupdated") {
-        echo '<p> Your Password has been reset!</p>';
-      }
+        if ($_GET["newpwd"] == "passwordupdated") {
+            echo '<p> Your Password has been reset!</p>';
+        }
     }
-      ?>
+    ?>
     <a href="reset-password.php">Forgot Your Password?</a>
 </section>
 
 <script>
-  function checkPasswordStrength() {
-    var password = document.getElementById("password").value;
-    var strengthIndicator = document.getElementById("password-strength");
+    function checkPasswordStrength() {
+        var password = document.getElementById("password").value;
+        var strengthIndicator = document.getElementById("password-strength");
 
-    // Reset strength indicator and color
-    strengthIndicator.innerHTML = "";
-    strengthIndicator.style.color = "";
+        // Reset strength indicator and color
+        strengthIndicator.innerHTML = "";
+        strengthIndicator.style.color = "";
 
-    // Minimum length requirement
-    if (password.length < 8) {
-      strengthIndicator.innerHTML = "Weak";
-      strengthIndicator.style.color = "red";
-      return;
+        // Minimum length requirement
+        if (password.length < 8) {
+            strengthIndicator.innerHTML = "Weak";
+            strengthIndicator.style.color = "red";
+            return;
+        }
+
+        // Check for uppercase letters, lowercase letters, and numbers
+        var hasUppercase = /[A-Z]/.test(password);
+        var hasLowercase = /[a-z]/.test(password);
+        var hasNumber = /\d/.test(password);
+
+        // Determine strength level
+        if (hasUppercase && hasLowercase && hasNumber) {
+            strengthIndicator.innerHTML = "Strong";
+            strengthIndicator.style.color = "green";
+        } else if (hasUppercase || hasLowercase || hasNumber) {
+            strengthIndicator.innerHTML = "Medium";
+            strengthIndicator.style.color = "orange";
+        } else {
+            strengthIndicator.innerHTML = "Weak";
+            strengthIndicator.style.color = "red";
+        }
     }
-
-    // Check for uppercase letters, lowercase letters, and numbers
-    var hasUppercase = /[A-Z]/.test(password);
-    var hasLowercase = /[a-z]/.test(password);
-    var hasNumber = /\d/.test(password);
-
-    // Determine strength level
-    if (hasUppercase && hasLowercase && hasNumber) {
-      strengthIndicator.innerHTML = "Strong";
-      strengthIndicator.style.color = "green";
-    } else if (hasUppercase || hasLowercase || hasNumber) {
-      strengthIndicator.innerHTML = "Medium";
-      strengthIndicator.style.color = "orange";
-    } else {
-      strengthIndicator.innerHTML = "Weak";
-      strengthIndicator.style.color = "red";
-    }
-  }
 </script>
 
 <style>
-  #password-strength {
-    display: block;
-    margin-top: 5px;
-    font-size: 14px;
-  }
+    #password-strength {
+        display: block;
+        margin-top: 5px;
+        font-size: 14px;
+    }
 </style>
 
 <?php
-  include 'footer.php';
+include 'footer.php';
 ?>
