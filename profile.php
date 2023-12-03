a28dbcc33aeca4e153928dc05fd8df41ce25c4ce
ErickMadatyan <89811461+ErickMadatyan@users.noreply.github.com>
2023-12-03 15:03:30 -0800

    Update profile.php

--- profile.php
+++ profile.php
@@ -5,7 +5,36 @@
   <body>  
   <div class="content-wrap">
     <div class="column-left">
-hello world
+      //last login
+<section class="profile-info">
+    <h2>Welcome to Your Profile</h2>
+
+    <?php
+    // Check if the user is logged in
+    if (isset($_SESSION['userid'])) {
+        $userId = $_SESSION['userid'];
+
+        // Fetch the last login timestamp from the database
+        $sql = "SELECT last_login FROM users WHERE usersId = $userId";
+        $result = mysqli_query($conn, $sql);
+
+        if ($result) {
+            $row = mysqli_fetch_assoc($result);
+            $lastLogin = $row['last_login'];
+
+            echo "<p>Last Login: $lastLogin</p>";
+        } else {
+            echo "<p>Error fetching last login information.</p>";
+        }
+    } else {
+        // Redirect the user to the login page if not logged in
+        header("Location: login.php");
+        exit();
+    }
+    ?>
+
+</section>
+      //end of last login
     </div>  
   </div>  
 
