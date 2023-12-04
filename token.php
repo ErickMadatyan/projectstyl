d36eb13372763390ba246d3ac15b3b5f0155b830
ErickMadatyan <89811461+ErickMadatyan@users.noreply.github.com>
2023-12-03 18:11:00 -0800

    Update token.php

--- token.php
+++ token.php
@@ -8,7 +8,7 @@
  <?php 
     if (isset($_GET["token"])) {
         require 'dbh.inc.php';
-        $sql = "UPDATE users SET email_verified=true WHERE token=?";
+        $sql = "UPDATE users SET email_verified=true WHERE verification_code=?";
         $stmt = mysqli_stmt_init($conn);
 if(!mysqli_stmt_prepare($stmt, $sql)) {
       echo "There was an error!";
