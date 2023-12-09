7bb9455455240d9f9066671acbc829425d8024a4
ErickMadatyan <89811461+ErickMadatyan@users.noreply.github.com>
2023-12-09 00:43:26 -0800

    Update index.php

--- index.php
+++ index.php
@@ -19,11 +19,11 @@
         <h2>Gallery</h2>
 
         <div class="gallery-container">
-<a href="#">
-  <div style="width: 100%; height: 140px; background-color: red;"></div>
-  <h3>This is a title</h3>
-  <p>This is a paragraph</p>
-</a>
+          <a href="#">
+            <div> </div>
+            <h3>This is a title</h3>
+            <p>This is a paragraph</p>
+          </a>
           <a href="#">
             <div> </div>
             <h3>This is a title</h3>
