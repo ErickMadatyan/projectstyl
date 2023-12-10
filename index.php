7ce8dfa74d7141f3a766620e5a23f7f001da0ad4
ErickMadatyan <89811461+ErickMadatyan@users.noreply.github.com>
2023-12-09 20:04:55 -0800

    Update index.php

--- index.php
+++ index.php
@@ -55,8 +55,11 @@
       $result = mysqli_stmt_get_result($stmt);
 
 while($row = mysqli_fetch_assoc($result)) {
-    echo "right here";
-
+          echo '<a href="#">
+            <div style="background-image: url(../../../../var/log/myapp/'.$row["imgFullNameGallery"].');"> </div>
+            <h3>'.$row["imageTitle"].'</h3>
+            <p>'.$row["hatDESC"].'</p>
+          </a>';
       }
     }
 
