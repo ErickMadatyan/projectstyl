169ff55c2f9db5ef130e22341d68d497853eac96
ErickMadatyan <89811461+ErickMadatyan@users.noreply.github.com>
2023-12-09 21:27:22 -0800

    Update index.php

--- index.php
+++ index.php
@@ -16,8 +16,8 @@
 }
 
 .gallery-container a div {
-  width: 100%;
-  height: 200px; /* Adjust the height according to your preference */
+  width: 200%;
+  height: 300px; /* Adjust the height according to your preference */
   background-size: cover;
   background-position: center;
 }
