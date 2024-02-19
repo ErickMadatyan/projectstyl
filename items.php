8ec800d479650a234eb2bce407526695cf80342e
ErickMadatyan <89811461+ErickMadatyan@users.noreply.github.com>
2024-02-19 15:53:08 -0800

    Update items.php

--- items.php
+++ items.php
@@ -82,7 +82,7 @@
     <div class="container">
         <?php
             include_once 'dbh.inc.php';
-            $sql = "SELECT * FROM gallery ORDER BY orderGallery DESC";
+            $sql = "SELECT * FROM gallery WHERE idGallery = '19'";
             $stmt = mysqli_stmt_init($conn);
             if (!mysqli_stmt_prepare($stmt, $sql)) {
               echo "SQL STATEMENT FAILED!";
