b7229cb51472beaa690f29243a63496f1e2ea9aa
ErickMadatyan <89811461+ErickMadatyan@users.noreply.github.com>
2024-02-19 15:04:31 -0800

    Update dbh.inc.php

--- dbh.inc.php
+++ dbh.inc.php
@@ -2,7 +2,7 @@
 $serverName = "db2102024.cr6vvrkufa98.us-east-2.rds.amazonaws.com";
 $dBUsername = "admin";
 $dbPassword = "Dark30death";
-$dBName = "projectstyldb";
+$dBName = "db2102024";
 $port = 3306; // Default port for MySQL
 
 // Create connection
