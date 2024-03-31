3f438e3935bc5691af26ce7f971742ad71aa48f6
ErickMadatyan <89811461+ErickMadatyan@users.noreply.github.com>
2024-03-31 15:34:19 -0700

    Update functions.inc.php

--- functions.inc.php
+++ functions.inc.php
@@ -89,7 +89,7 @@ function createUser($conn, $name, $email, $username, $pwd) {
     $mail->Host       = 'smtp.gmail.com';                     //Set the SMTP server to send through
     $mail->SMTPAuth   = true;                                   //Enable SMTP authentication
     $mail->Username   = 'project.styl.inc@gmail.com';                     //SMTP username
-    $mail->Password   = 'fwttnopuzhamhtry';                               //SMTP password
+    $mail->Password   = 'tmza wpgv evdq dgle';                               //SMTP password
     $mail->SMTPSecure = PHPMailer::ENCRYPTION_SMTPS;            //Enable implicit TLS encryption
     $mail->Port       = 465;                                    //TCP port to connect to; use 587 if you have set `SMTPSecure = PHPMailer::ENCRYPTION_STARTTLS`
 
