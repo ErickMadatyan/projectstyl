de94a92fddf21c25681c171d4329a850c6ce3dd7
ErickMadatyan <89811461+ErickMadatyan@users.noreply.github.com>
2024-05-12 11:52:54 -0700

    Update search.php

--- search.php
+++ search.php
@@ -2,6 +2,89 @@
 include 'header.php';
 ?>
 
+ <style>
+      .greeting {
+    text-align: center;
+    font-size: 1.5em; /* Adjust the font size as needed */
+    color: #007BFF; /* Adjust the color as needed */
+    padding-left: 200px;
+  }
+      .gallery-heading {
+    text-align: center;
+    margin-bottom: 20px; /* You can adjust this margin as needed */
+    font-siz
+  }
+      .gallery-heading h2 {
+    font-size: 2.5em; /* Adjust the font size as needed */
+    font-family: 'Your_Nice_Font', sans-serif; /* Replace 'Your_Nice_Font' with the desired font */
+    color: #007BFF; /* Adjust the color as needed */
+  }
+    body {
+      font-family: 'Open Sans', sans-serif;
+      margin: 0;
+      padding: 0;
+      box-sizing: border-box;
+    }
+
+    .gallery-container {
+      display: flex;
+      flex-wrap: wrap;
+      justify-content: space-around;
+    }
+
+    .gallery-container a {
+        border-radius: 25px;
+  border: 2px solid #73AD21;
+      flex: 0 0 32%;
+      margin-bottom: 20px;
+      text-decoration: none;
+      display: flex;
+      flex-direction: row;
+      border: 1px solid #ddd;
+      transition: transform 0.3s ease-in-out;
+      overflow: hidden;
+    }
+
+    .gallery-container a:hover {
+      transform: scale(1.05);
+    }
+
+    .gallery-container a .gallery-image {
+      width: 60%;
+      height: 500px;
+      background-size: contain;
+      background-position: center;
+      background-repeat: no-repeat;
+      background-color: #99ccff;
+    }
+
+    .gallery-container a .item-descriptions {
+      width: 40%;
+      padding: 20px;
+      box-sizing: border-box;
+      display: flex;
+      flex-direction: column;
+
+      overflow: auto;
+      color: #333;
+      background-color: #99ccff;
+    }
+
+    .gallery-container a h3 {
+      font-size: 1.8em;
+      margin-top: 0;
+      margin-bottom: 10px;
+      color: #007BFF;
+    }
+
+    .gallery-container a p {
+      margin: 5px 0;
+      font-size: 1em;
+      line-height: 1.4;
+    }
+  </style>
+</head>
+
 <div class="content-wrap">
     <div class="column-left">
     <section class="gallery-links">
