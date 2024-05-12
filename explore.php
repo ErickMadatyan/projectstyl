9812beb9b8a847015eaa3b21aa82a2d1db2e87de
ErickMadatyan <89811461+ErickMadatyan@users.noreply.github.com>
2024-05-12 13:18:44 -0700

    Update explore.php

--- explore.php
+++ explore.php
@@ -15,7 +15,7 @@ ini_set('display_errors', 1);
 
   .gallery-container .highest-voted,
   .gallery-container .newest-posts {
-    width: 48%; /* Adjust the width as needed */
+    width: 80%; /* Adjust the width as needed */
     margin-bottom: 20px;
   }
 
@@ -36,7 +36,7 @@ ini_set('display_errors', 1);
   }
 
   .gallery-container a .gallery-image {
-    width: 60%;
+    width: 30%;
     height: 200px; /* Adjust the height as needed */
     background-size: contain;
     background-position: center;
