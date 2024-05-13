779fb441208a22a5230bb4b3501178a97af372bb
ErickMadatyan <89811461+ErickMadatyan@users.noreply.github.com>
2024-05-12 23:16:26 -0700

    Update footerv2.php

--- footerv2.php
+++ footerv2.php
@@ -5,11 +5,11 @@
     <footer class="unique-footer">
         <div class="unique-footer-container">
             <div class="unique-footer-social-icons">
-                <a href=""><i class="fa-brands fa-facebook"></i></a>
-                <a href=""><i class="fa-brands fa-instagram"></i></a>
-                <a href=""><i class="fa-brands fa-twitter"></i></a>
-                <a href=""><i class="fa-brands fa-google-plus"></i></a>
-                <a href=""><i class="fa-brands fa-youtube"></i></a>
+                <a href="https://www.facebook.com/"><i class="fa-brands fa-facebook"></i></a>
+                <a href="https://www.instagram.com/"><i class="fa-brands fa-instagram"></i></a>
+                <a href="https://twitter.com/"><i class="fa-brands fa-twitter"></i></a>
+                <a href="https://www.google.com/"><i class="fa-brands fa-google-plus"></i></a>
+                <a href="https://www.youtube.com/"><i class="fa-brands fa-youtube"></i></a>
             </div>
 
         </div>
