2ddd60716c15c0535a6e8a5737ca4b6947da0ca0
ErickMadatyan <89811461+ErickMadatyan@users.noreply.github.com>
2024-05-12 23:14:47 -0700

    Update footerv2.php

--- footerv2.php
+++ footerv2.php
@@ -11,16 +11,10 @@
                 <a href=""><i class="fa-brands fa-google-plus"></i></a>
                 <a href=""><i class="fa-brands fa-youtube"></i></a>
             </div>
-            <div class="unique-footer-nav">
-                <ul>
-                    <li><a href="">Home</a></li>
-                    <li><a href="">News</a></li>
-                    <li><a href="">About</a></li>
-                    <li><a href="">Contact Us</a></li>
-                    <li><a href="">Our Team</a></li>
-                </ul>
-            </div>
-        </div>
 
+        </div>
+        <div class="unique-footer-bottom">
+            <p>Copyright &copy;2023; Project <span class="unique-footer-designer">STYL</span></p>
+        </div>
     </footer>
 </div>
